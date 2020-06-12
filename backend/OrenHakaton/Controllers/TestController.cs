67a9597ca1c952520bcdabddbe606ef60a602f52
r.ali-isys <r.ali-isys@i-sys.ru>
2020-06-12 18:35:38 +0500

    [+] #2 Созданы тестовые контроллеры

---
+++ backend/OrenHakaton/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿namespace OrenHakaton.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "asdsad", "asdsad", "Mild", "Warm", "Balmy", "asdsaddsa", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var rng = new Random();
+            var array = new List<string>();
+
+            array.Add("asdsaddsa");
+
+            return array;
+        }
+    }
+}
--- backend/OrenHakaton/Program.cs
+++ backend/OrenHakaton/Program.cs
@@ -1,10 +1,7 @@
 namespace OrenHakaton
 {
-    using System;
-    using System.Collections.Generic;
     using System.Linq;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Hosting;
     using OrenHakaton.Models;
 
@@ -14,33 +11,33 @@ namespace OrenHakaton
     {
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
-        public static void Main()
+        public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
 
-            using (var db = new OrenHakatonContext())
-            {
-                // Create
-                logger.Trace("Inserting a new Apartments");
-                db.Add(new Apartments { Number = "8888888" });
-                db.SaveChanges();
+            //using (var db = new OrenHakatonContext())
+            //{
+            //    // Create
+            //    logger.Trace("Inserting a new Apartments");
+            //    db.Add(new Apartments { Number = "8888888" });
+            //    db.SaveChanges();
 
-                // Read
-                logger.Trace("Querying for a Apartments");
-                var apartments = db.Apartments
-                    .OrderBy(b => b.ApartmentId)
-                    .First();
+            //    // Read
+            //    logger.Trace("Querying for a Apartments");
+            //    var apartments = db.Apartments
+            //        .OrderBy(b => b.ApartmentId)
+            //        .First();
 
-                // Update
-                logger.Trace("Updating the blog and adding a post");
-                apartments.Number = "1111111";
-                db.SaveChanges();
+            //    // Update
+            //    logger.Trace("Updating the blog and adding a post");
+            //    apartments.Number = "1111111";
+            //    db.SaveChanges();
 
-                // Delete
-                //logger.Trace("Delete the blog");
-                //db.Remove(blog);
-                //db.SaveChanges();
-            }
+            //    // Delete
+            //    //logger.Trace("Delete the blog");
+            //    //db.Remove(blog);
+            //    //db.SaveChanges();
+            //}
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
