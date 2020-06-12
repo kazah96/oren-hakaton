f5dc114f1c6cac0b318a759e7cde2ca4c1b7edc2
r.ali-isys <r.ali-isys@i-sys.ru>
2020-06-12 16:53:44 +0500

    [+] #2 Добавлена тестовая работа с БД

--- backend/OrenHakaton/OrenHakaton/Program.cs
+++ backend/OrenHakaton/OrenHakaton/Program.cs
@@ -1,19 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace OrenHakaton
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Hosting;
+    using OrenHakaton.Models;
+
+    using NLog;
+
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Main()
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            using (var db = new OrenHakatonContext())
+            {
+                // Create
+                logger.Trace("Inserting a new Apartments");
+                db.Add(new Apartments { Number = "8888888" });
+                db.SaveChanges();
+
+                // Read
+                logger.Trace("Querying for a Apartments");
+                var apartments = db.Apartments
+                    .OrderBy(b => b.ApartmentId)
+                    .First();
+
+                // Update
+                logger.Trace("Updating the blog and adding a post");
+                apartments.Number = "1111111";
+                db.SaveChanges();
+
+                // Delete
+                //logger.Trace("Delete the blog");
+                //db.Remove(blog);
+                //db.SaveChanges();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
