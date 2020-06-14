535da831280e91ebc280ade408275efc5d2252ed
r.ali-isys <r.ali-isys@i-sys.ru>
2020-06-14 05:42:08 +0500

    [+] Обновлены маппинги.

--- backend/OrenHakaton/Mappings/MappingProfile.cs
+++ backend/OrenHakaton/Mappings/MappingProfile.cs
@@ -9,26 +9,42 @@
     {
         public MappingProfile()
         {
-            CreateMap<UsersDto, Users>();
-            CreateMap<Users, UsersDto>();
-
-            CreateMap<Requests, RequestsDto>();
-            CreateMap<RequestsDto, Requests>();
-
-            CreateMap<Houses, HousesDto>();
-            CreateMap<HousesDto, Houses>();
-
-            CreateMap<EmployeesMCDto, EmployeesMC>().ForMember(opt => opt.Specialties, dto => dto.MapFrom(src => src.Specialties));
-            CreateMap<EmployeesMC, EmployeesMCDto>().ForMember(dto => dto.Specialties, opt => opt.MapFrom(src => src.Specialties));
-
             CreateMap<Specialties, SpecialtiesDto>();
             CreateMap<SpecialtiesDto, Specialties>();
 
-            CreateMap<Meetings, MeetingsDto>();
-            CreateMap<MeetingsDto, Meetings>();
+            CreateMap<UsersDto, Users>()
+                .ForMember(opt => opt.Apartments, dto => dto.MapFrom(src => src.Apartments));
+            CreateMap<Users, UsersDto>()
+                .ForMember(dto => dto.Apartments, opt => opt.MapFrom(src => src.Apartments));
 
-            CreateMap<ManagementCompanies, ManagementCompaniesDto>();
-            CreateMap<ManagementCompaniesDto, ManagementCompanies>();
+            CreateMap<Requests, RequestsDto>()
+                .ForMember(opt => opt.ManagementCompanies, dto => dto.MapFrom(src => src.ManagementCompanies));
+            CreateMap<RequestsDto, Requests>()
+                .ForMember(dto => dto.ManagementCompanies, opt => opt.MapFrom(src => src.ManagementCompanies));
+
+            CreateMap<Houses, HousesDto>()
+                .ForMember(opt => opt.Apartments, dto => dto.MapFrom(src => src.Apartments));
+            CreateMap<HousesDto, Houses>()
+                .ForMember(dto => dto.Apartments, opt => opt.MapFrom(src => src.Apartments));
+
+            CreateMap<ManagementCompanies, ManagementCompaniesDto>()
+                .ForMember(opt => opt.Houses, dto => dto.MapFrom(src => src.Houses));
+            CreateMap<ManagementCompaniesDto, ManagementCompanies>()
+                .ForMember(dto => dto.Houses, opt => opt.MapFrom(src => src.Houses));
+
+            CreateMap<EmployeesMCDto, EmployeesMC>()
+                .ForMember(opt => opt.Specialties, dto => dto.MapFrom(src => src.Specialties))
+                .ForMember(opt => opt.ManagementCompanies, dto => dto.MapFrom(src => src.ManagementCompanies));
+            CreateMap<EmployeesMC, EmployeesMCDto>()
+                .ForMember(dto => dto.Specialties, opt => opt.MapFrom(src => src.Specialties))
+                .ForMember(dto => dto.ManagementCompanies, opt => opt.MapFrom(src => src.ManagementCompanies));
+
+            CreateMap<Meetings, MeetingsDto>()
+                .ForMember(opt => opt.Houses, dto => dto.MapFrom(src => src.Houses))
+                .ForMember(opt => opt.ManagementCompanies, dto => dto.MapFrom(src => src.ManagementCompanies));
+            CreateMap<MeetingsDto, Meetings>()
+                .ForMember(dto => dto.Houses, opt => opt.MapFrom(src => src.Houses))
+                .ForMember(dto => dto.ManagementCompanies, opt => opt.MapFrom(src => src.ManagementCompanies));
         }
     }
 }
